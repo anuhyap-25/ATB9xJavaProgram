83fbe67b0b56341ad4f0fbb7b41e2779236e80ba
Anuhya <anuhya.pachava@gmail.com>
2024-10-16 22:56:56 +0530

    All files 16th oct

--- src/Task/task_16102024/Task2.java
+++ src/Task/task_16102024/Task2.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println(args[0]);
-        System.out.println(args[1]);
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int max = a>b ? a:b;
